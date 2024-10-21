97814655419a862b8ee123b1a597112d2dc36c4a
Alejandro Briones <alejandrobriava26_15@outlook.com>
2024-10-21 14:29:16 -0600

    Delete ejemplo0.php

--- ejemplo0.php
+++
@@ -1,7 +0,0 @@
-<php 
- echo "Mi nombre es:Luis ALonso Garcia Ramos<BR>";
- echo "Mi edad es: 16 años <BR>";
- echo "Mi hobbie es jugar <BR>";
- echo "El profe que me cae mal es la de humanidades <BR>";
- echo "La que me gusta es brenda de la mañana <BR>"
-?>
